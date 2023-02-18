cd817f75e599e1f1b56508e4cef78bfd42ce68a1
Paul Haacker <st175547@stud.uni-stuttgart.de>
2023-02-18 10:44:30 -0800

    Fixed Basic Simulation

--- simulation_basic.m
+++ simulation_basic.m
@@ -3,36 +3,38 @@ clear
 
 %% ------ parameters
 
-% [Schmidt17]
-A = 2; % max age
-mu = @(a) .1; % mortality rate fcn
-k = @(a) 2*a.*(A-a); % birth kernel
-p = 1; % output kernel
-manuallyProvideMuINT = false; % boolean, that switches integral of mu on or off.
-D_star = 1; % steady-state dilution rate
-y0 = 1; % initial output
-c1 = -.066;
-c2 = -.9;
-x0 = @(a) c1*a + exp(c2*a); % IC
-sigma(1) = -4.0335; % eigenvalues of the form lambda = -sigma/A+-j*omega/(2*pi*A)
-omega(1) = 55.4606;
-sigma(2) = -4.9866;
-omega(2) = 95.7048;
-
-% % [KurthSawodny21]
+% % [Schmidt17]
 % A = 2; % max age
-% mu = @(a) 1/(20-5*a); % mortality rate function - problem: matlab cannot find the correct integral...
-% k = @(a) a; % birth kernel
+% mu = @(a) .1; % mortality rate fcn
+% k = @(a) 2*a.*(A-a); % birth kernel
 % p = 1; % output kernel
-% manuallyProvideMuINT = true; % boolean, that switches integral of mu on or off.
-% mu_int = @(a) -log((4-a)/4)/5; % = int_0^a mu(s) ds for a \in [0,2]
-% D_star = 0.4837;
+% manuallyProvideMuINT = false; % boolean, that switches integral of mu on or off.
+% D_star = 1; % steady-state dilution rate
 % y0 = 1; % initial output
-% x0 = @(a) 8-3*a; % IC
-% sigma(1) = -1.8224; % eigenvalues of the form lambda = -sigma/A+-j*omega/(2*pi*A)
-% omega(1) = 48.0574;
-% sigma(2) = -2.3838;
-% omega(2) = 87.8539;
+% c1 = -.066;
+% c2 = -.9;
+% x0 = @(a) c1*a + exp(c2*a); % IC
+% sigma(1) = -4.0335; % eigenvalues of the form lambda = -sigma/A+-j*omega/(2*pi*A)
+% omega(1) = 55.4606;
+% sigma(2) = -4.9866;
+% omega(2) = 95.7048;
+
+% [KurthSawodny21]
+A = 2; % max age
+mu = @(a) 1./(20-5*a); % mortality rate function - problem: matlab cannot find the correct integral...
+k = @(a) a; % birth kernel
+p = @(a) 1+.1*a.^2; % output kernel
+manuallyProvideMuINT = true; % boolean, that switches integral of mu on or off.
+mu_int = @(a) -log((4-a)/4)/5; % = int_0^a mu(s) ds for a \in [0,2]
+D_star = 0.4837;
+y0 = 1; % initial output
+x0 = @(a) (8 - 3*a); % IC
+sigma(1) = -1.8224; % eigenvalues of the form lambda = -sigma/A+-j*omega/(2*pi*A)
+omega(1) = 48.0574;
+sigma(2) = -2.3838;
+omega(2) = 87.8539;
+
+%% stash of unordered parameter sets
 
 % % parameters for IC - Thesis [Schmidt16]
 % mu_par = 1.3;
@@ -46,10 +48,13 @@ omega(2) = 95.7048;
 % x0_par = 6/A^2/nu_par^3/(1-exp(-nu_par*A))*(A*nu_par*exp(-A*nu_par)+2*exp(-A*nu_par)-2+A*nu_par+2*y0*nu_par);
 % x0 = @(a) (-b_par*a + x0_par * exp(-nu_par*a));
 
+%% debug
+
 N_EV = length(sigma); % number of nonzero eigenvalues considered
 
 sign_ImaginaryPart = 1; % only works for +1
 EV = -sigma/A + 1i*omega/(2*pi*A)*sign_ImaginaryPart;
+
 
 %% ------ find integral of mortality rate symbolically
 
@@ -70,7 +75,7 @@ parameter.k = k; % birth kernel - function handle
 parameter.p = p; % output kernel - double
 parameter.D_star = D_star; % steady-state dilution rate - double
 
-% parameters for IC - paper [Schmidt17]
+% parameters for IC
 parameter.x0 = x0; % function handle
 
 % eigenvalues of the form lambda = -sigma/A+-j*omega/(2*pi*A)
@@ -147,7 +152,7 @@ axes_handle.CameraPosition = [15.8393  -65.0215   37.0943];
 % choose desired setpoint for output - equivalent to choosing a desired
 % equilibrium profile x^\ast(a), or better its family parameter.
 
-y_des = .5;
+y_des = 12;
 
 % D_ctrl = @(lambda) D_star + log(C_mat*lambda/y_des);
 D_ctrl = @(lambda) D_star + (C_mat*lambda-y_des)/y_des;
@@ -156,7 +161,7 @@ dynamics = @(t,lambda) (A_mat-eye(size(A_mat))*D_ctrl(lambda))*lambda;
 
 lambda_0 = zeros(size(A_mat,1),1);
 lambda_0(end) = 1;
-tspan = [0 20];
+tspan = [0 6];
 
 [t_sample,lambda_sample] = ode45(dynamics,tspan,lambda_0);
 
@@ -169,10 +174,11 @@ end
 
 %% plot results - P-controller stabilizing setpoint
 figure
-tiledlayout(2,2)
+tiles_handle = tiledlayout(2,2);
+title(tiles_handle,'Population Dynamics with P-controller stabilizing setpoint','Interpreter','Latex')
 nexttile
 plot(t_sample,lambda_sample)
-title('discretized states $\lambda(t)$ - logarithmic P-controller stabilizing setpoint')
+title('discretized states $\lambda(t)$')
 xlabel('time t')
 grid on
 
@@ -180,8 +186,8 @@ nexttile
 hold on
 plot(t_sample,ones(size(y_sample))*y_des,'--k','Linewidth',1.5)
 plot(t_sample,y_sample)
-title('output $y(t)$ - logarithmic P-controller stabilizing setpoint')
-legend('desired output $y_{des}$','output $y(t)$')
+title('output $y(t)$')
+legend('desired output $y_\mathrm{des}$','output $y(t)$')
 xlabel('time $t$')
 grid on
 
@@ -189,7 +195,7 @@ nexttile
 hold on
 plot(t_sample,ones(size(D_sample))*D_star,'--k','Linewidth',1.5)
 plot(t_sample,D_sample)
-title('control input $D(t)$ - logarithmic P-controller stabilizing setpoint')
+title('control input $D(t)$')
 legend('steady state input $D^\ast$','input $D(t)$')
 xlabel('time $t$')
 grid on
@@ -210,13 +216,67 @@ for ii = 1:length(t_sample)
 end
 
 axes_handle = nexttile;
-surf_plot = surf(a_mesh,t_mesh,x_mesh,'FaceColor',[0 0.4470 0.7410]);
+% surf_plot = surf(a_mesh,t_mesh,x_mesh,'FaceColor',[0 0.4470 0.7410]);
+surf_plot = surf(a_mesh,t_mesh,x_mesh,'FaceColor','none');
 LessEdgeSurf(surf_plot,10,10);
 axes_handle.CameraPosition = [15.8393  -65.0215   37.0943];
 
 xlabel('age $a$')
 ylabel('time $t$')
-title('population density $x(t,a)$ - logarithmic P-controller stabilizing setpoint')
+title('population density $x(t,a)$')
+
+%% plot results - P-controller stabilizing setpoint - DIAGNE plot
+fig_handle = figure;
+sgtitle('Population Dynamics with P-controller stabilizing setpoint','Interpreter','Latex')
+
+% nexttile
+% plot(t_sample,lambda_sample)
+% title('discretized states $\lambda(t)$')
+% xlabel('time t')
+% grid on
+
+ax1 = subplot(2,6,1:3);
+hold on
+plot(t_sample,ones(size(y_sample))*y_des,'--k','Linewidth',1.5)
+plot(t_sample,y_sample)
+title('output $y(t)$')
+legend('desired output $y_\mathrm{des}$','output $y(t)$')
+xlabel('time $t$')
+grid on
+
+ax2 = subplot(2,6,4:6);
+hold on
+plot(t_sample,ones(size(D_sample))*D_star,'--k','Linewidth',1.5)
+plot(t_sample,D_sample)
+title('control input $D(t)$')
+legend('steady state input $D^\ast$','input $D(t)$')
+xlabel('time $t$')
+grid on
+
+% plot the PDE state where x(t,a) = lambda(t)'*phi(a);
+
+% time sample from above
+% define domain sample
+a_sample = 0:0.1:A;
+
+[a_mesh,t_mesh] = meshgrid(a_sample,t_sample);
+x_mesh = zeros(size(a_mesh));
+
+for ii = 1:length(t_sample)
+    for jj = 1:length(a_sample)
+        x_mesh(ii,jj) = lambda_sample(ii,:)*eval_phi(phi,a_sample(jj));
+    end
+end
+
+axes_handle = subplot(2,6,8:11);
+% surf_plot = surf(a_mesh,t_mesh,x_mesh,'FaceColor',[0 0.4470 0.7410]);
+surf_plot = surf(a_mesh,t_mesh,x_mesh,'FaceColor','none');
+LessEdgeSurf(surf_plot,20,20);
+axes_handle.CameraPosition = [15.8393  -65.0215   37.0943];
+
+xlabel('age $a$')
+ylabel('time $t$')
+title('population density $f(t,a)$')
 
 
 %% functions
